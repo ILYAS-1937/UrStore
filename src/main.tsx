d28f273942806af95057c1f0b6cb80f71e747b1e
TARZI ILYAS <tarziilyas8@gmail.com>
2026-05-06 00:39:08 +0100

    final fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-
+// On détecte si le type de navigation est un rechargement (refresh)
+const navEntries = performance.getEntriesByType("navigation");
+if (navEntries.length > 0 && (navEntries[0] as PerformanceNavigationTiming).type === "reload") {
+  // Si on n'est pas déjà sur la page d'accueil, on force la redirection
+  if (window.location.pathname !== '/') {
+    window.location.replace('/');
+  }
+}
+// --------------------------
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
